8620386c5aac5532ca50357feb07881098d02cce
richardliuu <richardliu200127@gmail.com>
2025-05-02 21:58:13 -0400

    audio data graphing

--- model.py
+++ model.py
@@ -26,7 +26,7 @@ def plot_signals(signals):
 def plot_fft(fft):
     fig, axes= plt.subplots(nrows=2, ncols=5, sharex=False, sharey=True, figsize=(20,5))
 
-    fig.suptitle('Fourier Transofmr', size=16)
+    fig.suptitle('Fourier Transform', size=16)
     i = 0 
     for x in range(2):
         for y in range(5):
@@ -39,4 +39,31 @@ def plot_fft(fft):
             i += 1
 
 def plot_fbank(fbank):
-    pass 
+    fig, axes = plt.subplots(nrows=2, ncols=5, sharex=False, sharey=True, figsize=(20, 5))
+    fig.suptitle("Filter Bank Coefficients", size=16)
+    i=0
+    for x in range(2):
+        for y in range(5):
+            axes[x,y].set_title(list(fbank.keys())[i])
+            axes[x,y].imshow(list(fbank.values())[i],
+                             cmap='hot', interpolations='nearest')
+            axes[x,y].get_xaxis().set_visible(False)
+            axes[x,y].get_yaxis().set_visible(False)
+            i += 1
+
+# Mel frequency cepstral coefficients 
+def plot_mfccs(mfccs):
+    fig, axes = plt.subplots(nrows=2, ncols=5, sharex=False, sharey=False, figsize=(20,5))
+    
+    fig.suptitle("Mel Frequency Cepstral Coefficients", size=16)
+    i=0
+    for x in range(2):
+        for y in range(5):
+            axes[x,y].set_title(list(mfccs.keys())[i])
+            axes[x,y].imshow(list(mfccs.values())[i],
+                             cmap='hot', interpolations='nearest')
+            axes[x,y].get_xaxis().set_visible(False)
+            axes[x,y].get_yaxis().set_visible(False)
+            i += 1
+
+df = pd.read_csv('')
