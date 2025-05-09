c0dc1a20bdb71474fd9a05b53047fd9fdd36dcaa
richardliuu <richardliu200127@gmail.com>
2025-05-08 22:47:23 -0400

    graphing file

--- classdistribution.py
+++ classdistribution.py
@@ -4,9 +4,8 @@ import pandas as pd
 
 # Insert values 
 data = {
-    'Stage': ("Healthy", "Stage 1", "Stage 2", "Stage 3", "Stage 4"), 
-    'Value': ("")
-
+    'Stage': ["Healthy", "Stage 1", "Stage 2", "Stage 3", "Stage 4"], 
+    'Number of Diagnosis': [],
 }
 
 df = pd.DataFrame(data)
@@ -14,8 +13,8 @@ df = pd.DataFrame(data)
 sns.set_theme()
 
 plt.figure(figsize=(6, 6))
-plt.pie(df["Percentage"], labels=df[], autopct='%1.1f%%', startangle=140)
+plt.pie(df["Number of Diagnosis"], labels=df["Stage"], autopct='%1.1f%%', startangle=140)
 plt.title("Class Distribution of Lung Cancer Stages")
-
+plt.axis('equal')   
 plt.show()
 
