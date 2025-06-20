3cc6b3c411fa1474a13aa83b00fa392a98316f62
richardliuu <richardliu200127@gmail.com>
2025-06-19 20:58:36 -0400

    class based cnn

---
+++ class_based_models/lung_cancer_cnn.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import numpy as np
+import tensorflow as tf 
+import matplotlib.pyplot as plt
+import random
+import os
+from sklearn.model_selection import GroupKFold, train_test_split
+from sklearn.preprocessing import StandardScaler, LabelEncoder
+from sklearn.metrics import classification_report, confusion_matrix, roc_auc_score
+from tensorflow.keras.models import Sequential 
+from tensorflow.keras.layers import Dense, Dropout, BatchNormalization, Input 
+from tensorflow.keras.utils import to_categorical 
+from tensorflow.keras.callbacks import EarlyStopping, ReduceLROnPlateau 
+from imblearn.combine import SMOTEENN
+
+# Seed setting
+os.environ['PYTHONHASHSEED'] = '42'
+os.environ['TF_DETERMINISTIC_OPS'] = '1'
+
+SEED = 42
+np.random.seed(SEED)
+random.seed(SEED)
+tf.random.set_seed(SEED)
+tf.config.threading.set_intra_op_parallelism_threads(1)
+tf.config.threading.set_inter_op_parallelism_threads(1)
+
+class DataHandling:
+    def __init__(self, data=r"binary_mfccs.npy"):
+        # Instantiated all variables used in the CNN models 
+        pass
+
+class LungCancerCNN:
+    def __init__():
+        pass
