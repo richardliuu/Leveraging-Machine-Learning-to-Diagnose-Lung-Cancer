05f32923edd1965ed8c5b9cb9e54fd3a17e705d8
richardliuu <richardliu200127@gmail.com>
2025-10-05 16:58:46 -0400

    drafted into modular components

--- class_based_models/class_surrogate.py
+++ class_based_models/class_surrogate.py
@@ -21,6 +21,8 @@ Load data and analyze two specific cluster types from the models behaviour:
 Split the clusters into datasets for training through a surrogate model 
 
 NOTE Challenges: Running model on each dataset respectively 
+    - May just need one train function but call it on 3 different datasets
+
 
 Keep the surrogate model consistent which is either going to be:
 - Decision tree
@@ -29,9 +31,6 @@ Keep the surrogate model consistent which is either going to be:
 - Polynomial Regressor
 
 Then, generate UMAP projections to visualize the data 
-
-
-
 """
 
 # Import dependencies 
@@ -47,7 +46,16 @@ from sklearn.pipeline import Pipeline
 import umap
 import joblib
 import warnings
+import random
+import os 
 warnings.filterwarnings('ignore')
+
+# Seed Setting for reproducable results
+
+SEED = 42
+np.random.seed(SEED)
+random.seed(SEED)
+os.environ['PYTHONHASHSEED'] = str(SEED)
 
 class LoadData:
     """
@@ -95,51 +103,184 @@ class LoadData:
 
 class ClusterData:
     """
-    
+    This class module clusters data based on the model's 
+    confidence 
     """
     def __init__(self):
         """ Instantiate variables """
-
-        # Should probably make these variables dataframes so that they can be csv datasets 
-        self.confident = []
-        self.mixed = []
-        self.unconfident = [] 
+        super().__init__(LoadData)
+        self.confident = pd.DataFrame()
+        self.mixed = pd.DataFrame()
+        self.unconfident = pd.DataFrame()
 
     def cluster(self):
         """
-        
+        This function clusters the data and creates seperate datasets. 
         """
-
-        
+         
+        if self.rf_probs > 0.7:
+            self.confident.append(self.rf_probs)
+        elif self.rf_probs > 0.4 and self.rf_probs < 0.7:
+            self.mixed.append(self.rf_probs)
+        elif self.rf_probs < 0.4:
+            self.unconfident.append(self.rf_probs)
+        else:
+            pass
+  
 class SurrogateModel:
     """
-    
+    Base class for running our surrogate model.
+
+    The SurrogateModel class provides an object to call the model for training
+    when needed. 
+
+    The surrogate serves as a transparent and simplified representation of 
+    the random forest model used to predict lung cancer. This improves model
+    explainability and interpretability through the DecisionTreeRegressor's simpler 
+    rule-based structure.
     """ 
     def __init__(self):
         """ Instantiate variables """
-        pass 
+        self.model = None
+        self.model_params = None 
+        self.max_depth = None
 
-    def train(self):
+    def initialize_model(self, 
+                         max_depth = 4,
+                         min_samples_leaf = 20, 
+                         random_state = SEED
+                         ):
+        """
+        This function initializes the model and takes in
+        the hyperparameters for the DecisionTreeRegressor.
+        
+        Parameters:
+        ---------------
+        
+        max_depth = 4 
+            - Reduce complexity of our surrogate which aims to 
+            provide interpretability 
+
+        min_samples_leaf = 20 
+            - Ensure the model splits based on
+            a minimum sample size of 20
+
+        random_state = SEED 
+            - Ensure reproducable results when training 
+
+        Returns: self.model
+
+            The initialized decision tree regressor as an object
+        """
+
+        self.model = DecisionTreeRegressor(
+            max_depth = max_depth,
+            min_samples_leaf = min_samples_leaf,
+            random_state = random_state,
+        )
+
+        return self.model
+
+class TrainModel(SurrogateModel):
+    """
+    This class inherits from the parent class SurrogateModel() for training the model
+    on clustered datasets. 
+
+    The TrainModel class extends the functionality of SurrogateModel 
+    by providing methods to train the surrogate decision tree on input data.
+    """
+
+    def __init__(self):
+        """ Instantiate variables """
+        super().__init__()
+        self.performance_metrics = {}
+        self.predictions = None
+        self.X = None
+        self.y = None
+
+    def train(self, X, y):
+        """
+        Trains the underlying model using the 
+        provided feature matrix X and target vector y.
+
+        Parameters:
+            X (array-like): Feature matrix used for training.
+            y (array-like): Target values corresponding to X.
+        Returns:
+            self.model: The trained model instance.
         """
         
+        self.model.fit(X, y)
+        return self.model
+    
+    def predict(self, X):
         """
-        pass
+        Generate predictions for the input data X using the trained model.
+        Parameters:
+            X (array-like): Input features for which predictions are to be made.
 
-class TrainModel:
+        Returns:
+            predictions: Predicted values corresponding to the input data.
+
+        """
+        self.predictions = self.model.predict(X)
+        return self.predictions
+    
+class CrossValidation(SurrogateModel, TrainModel):
     """
     
     """
+
     def __init__(self):
-        super().__init__(SurrogateModel)
+        """
+        
+        """
+        super().__init__()
+        pass
 
+    def pipeline(self):
+        """
+        run the cross validation here
+        """
 
-class EvaluateTraining:
+        pass
+
+class EvaluateTraining(SurrogateModel, TrainModel):
     """
     
     """
     def __init__(self):
         """ Instantiate variables """
-        super().__init__(TrainModel)
+        super().__init__()
+        self.predictions = TrainModel.predict(X)
+
+    def evaluate(self):
+        """
+        Evaluates the performance of the model's predictions using common regression metrics.
+        Calculates and stores the following metrics in the `self.metrics` dictionary:
+            - Mean Absolute Error (mae)
+            - Mean Squared Error (mse)
+            - R-squared Score (r2)
+            - Root Mean Squared Error (rmse)
+
+        Returns:
+            self.metrics: A dictionary containing the computed regression metrics.
+        """
+
+        self.metrics = {
+            "mae": mean_absolute_error(self.y, self.predictions),
+            "mse": mean_squared_error(self.y, self.predictions),
+            "r2": r2_score(self.y, self.predictions),
+            "rmse": np.sqrt(mean_squared_error(self.y, self.predictions))
+        }
+
+        return self.metrics
+    
+class ModelReport():
+    """
+    
+    """
+    def __init__(self):
         pass
 
 class UMAPProjection:
